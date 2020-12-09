bb38e9890be6809ad0533c02fca5e12fdbedf1f4
Bac <ngonhatdongho>
2020-12-09 11:19:29 +0700

    Initial Commit

---
+++ demoSeachBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoSeachBar
+//
+//  Created by BAC Vuong Toan (VTI.Intern) on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
